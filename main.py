4959c76662c5f8e64524751b5ef5952499c34b52
repattila <repattila@protonmail.com>
2023-12-18 06:22:23 +0100

    Added solver for day 17, but it's too slow

--- main.py
+++ main.py
@@ -1226,7 +1226,7 @@ def solve12():
 
     print(sumValidArrangaments)
 
-import re
+import regex as re
 
 def findOptions(line, alreadyMatched, matchPos, pattern, optionCount):
     if len(line) <= matchPos:
@@ -1249,6 +1249,24 @@ def findOptions(line, alreadyMatched, matchPos, pattern, optionCount):
         else:
             return findOptions(line, alreadyMatched + line[matchPos], matchPos + 1, pattern, optionCount)
 
+def findOptionsWCheckSums(line, alreadyMatched, matchPos, checkSums, currCheck, optionCount):
+    if len(line) <= matchPos:
+        return optionCount
+    else:
+
+
+        if line[matchPos] == '?':
+            restOfLine = line[matchPos + 1:]
+            option1 = alreadyMatched + '.'
+            option2 = alreadyMatched + '#'
+            option1Count = 0
+            option2Count = 0
+
+
+
+            return option1Count + option2Count
+        else:
+            return findOptions(line, alreadyMatched + line[matchPos], matchPos + 1, pattern, optionCount)
 
 def solve12_alt():
     f = open("input12.txt", "r")
@@ -1287,6 +1305,21 @@ def solve12_alt():
         sum += optionsCount
 
     print(sum)
+
+def solve12_alt1():
+    f = open("input12.txt", "r")
+    rawLines = f.readlines()
+
+    lines = []
+    checkSums = []
+
+    for rawLine in rawLines:
+        splitLine = rawLine.strip().split(' ')
+        lines.append(splitLine[0])
+        checkSums.append([int(c) for c in splitLine[1].split(',')])
+
+    print(lines)
+    print(checkSums)
 
 def solve12_2():
     f = open("input12.txt", "r")
@@ -1762,11 +1795,146 @@ def solve16():
 
     print(maxEnergizedCount)
 
+def followRoute(field, cheapestRoute, currRow, currCol, dir, stepsInDir, heatLoss):
+    endRow = len(field) - 1
+    endCol = len(field[currRow]) - 1
+
+    if currRow == endRow and currCol == endCol:
+        return
+
+    moveDown = False
+    moveLeft = False
+    moveRight = False
+    moveUp = False
+
+    if dir == 'd':
+        if stepsInDir == 3:
+            moveLeft = True
+            moveRight = True
+        else:
+            moveLeft = True
+            moveRight = True
+            moveDown = True
+    elif dir == 'u':
+        if stepsInDir == 3:
+            moveLeft = True
+            moveRight = True
+        else:
+            moveLeft = True
+            moveRight = True
+            moveUp = True
+    elif dir == 'r':
+        if stepsInDir == 3:
+            moveDown = True
+            moveUp = True
+        else:
+            moveDown = True
+            moveUp = True
+            moveRight = True
+    elif dir == 'l':
+        if stepsInDir == 3:
+            moveDown = True
+            moveUp = True
+        else:
+            moveDown = True
+            moveUp = True
+            moveLeft = True
+
+    if moveDown:
+        nextRow = currRow + 1
+        nextCol = currCol
+
+        if nextRow == endRow and nextCol == endCol:
+            moveUp = False
+            moveRight = False
+            moveLeft = False
+
+    if moveRight:
+        nextRow = currRow
+        nextCol = currCol + 1
+
+        if nextRow == endRow and nextCol == endCol:
+            moveUp = False
+            moveDown = False
+            moveLeft = False
+
+    if moveDown:
+        nextRow = currRow + 1
+        nextCol = currCol
+        nextDir = 'd'
+        nextStepsInDir = 1 if nextDir != dir else stepsInDir + 1
+
+        if nextRow < len(field):
+            nextHeatLoss = heatLoss + field[nextRow][nextCol]
+            if cheapestRoute[nextRow][nextCol][1] != 0 and cheapestRoute[nextRow][nextCol][1] < nextHeatLoss:
+                pass
+            else:
+                cheapestRoute[nextRow][nextCol][1] = nextHeatLoss
+                followRoute(field, cheapestRoute, nextRow, nextCol, nextDir, nextStepsInDir, nextHeatLoss)
+
+    if moveRight:
+        nextRow = currRow
+        nextCol = currCol + 1
+        nextDir = 'r'
+        nextStepsInDir = 1 if nextDir != dir else stepsInDir + 1
+
+        if nextCol < len(field[nextRow]):
+            nextHeatLoss = heatLoss + field[nextRow][nextCol]
+            if cheapestRoute[nextRow][nextCol][2] != 0 and cheapestRoute[nextRow][nextCol][2] < nextHeatLoss:
+                pass
+            else:
+                cheapestRoute[nextRow][nextCol][2] = nextHeatLoss
+                followRoute(field, cheapestRoute, nextRow, nextCol, nextDir, nextStepsInDir, nextHeatLoss)
+
+    if moveLeft:
+        nextRow = currRow
+        nextCol = currCol - 1
+        nextDir = 'l'
+        nextStepsInDir = 1 if nextDir != dir else stepsInDir + 1
+
+        if 0 <= nextCol:
+            nextHeatLoss = heatLoss + field[nextRow][nextCol]
+            if cheapestRoute[nextRow][nextCol][3] != 0 and cheapestRoute[nextRow][nextCol][3] < nextHeatLoss:
+                pass
+            else:
+                cheapestRoute[nextRow][nextCol][3] = nextHeatLoss
+                followRoute(field, cheapestRoute, nextRow, nextCol, nextDir, nextStepsInDir, nextHeatLoss)
+
+    if moveUp:
+        nextRow = currRow - 1
+        nextCol = currCol
+        nextDir = 'u'
+        nextStepsInDir = 1 if nextDir != dir else stepsInDir + 1
+
+        if 0 <= nextRow:
+            nextHeatLoss = heatLoss + field[nextRow][nextCol]
+            if cheapestRoute[nextRow][nextCol][0] != 0 and cheapestRoute[nextRow][nextCol][0] < nextHeatLoss:
+                pass
+            else:
+                cheapestRoute[nextRow][nextCol][0] = nextHeatLoss
+                followRoute(field, cheapestRoute, nextRow, nextCol, nextDir, nextStepsInDir, nextHeatLoss)
+
+def solve17():
+    f = open("input17.txt", "r")
+    rawLines = f.readlines()
+
+    field = [[int(ch) for ch in line.strip()] for line in rawLines]
+    # array of cheapest route per direction per pos: [u, d, r, l]
+    cheapestRoute = [[[0, 0, 0, 0] for _ in line.strip()] for line in rawLines]
+
+    print(field)
+    print(cheapestRoute)
+
+    followRoute(field, cheapestRoute, 0, 0, 'r', 0, 0)
+
+    for row in cheapestRoute:
+        print(row)
+
 import sys
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    sys.setrecursionlimit(20000)
-    solve16()
+    sys.setrecursionlimit(200000)
+    solve17()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
